f4dd9026ab0e768c010a2546710ded0bc022dae4
Mike Lay <michael.lay.email@gmail.com>
2013-03-25 10:51:11 -0700

    Modernize Block for Current Best Practices
    Modernize block for current best practices
    
    - Change edit.php and add.php to use `$this->inc()` method
    - Remove unneeded variables
    - Add `public` decalation to methods
    - Convert short tage
    - Remove closing `?>` at end of file
    - Whitespace

--- packages/login/blocks/login/form_setup_html.php
+++ packages/login/blocks/login/form_setup_html.php
@@ -1,4 +1,4 @@
-<?php   defined('C5_EXECUTE') or die(_("Access Denied.")); ?> 
+<?php defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
 
 <script>
 function registerTextShown(cb){
@@ -9,22 +9,22 @@ function registerTextShown(cb){
 </script>
 
 <div style="margin-bottom:0px;">
-<input name="showRegisterLink" type="checkbox" value="1" <?php  echo ($bObj->showRegisterLink)?'checked':''?> onchange="registerTextShown(this)" onclick="registerTextShown(this)" /> 
-<?php  echo t('Show Register Link') ?>
+<input name="showRegisterLink" type="checkbox" value="1" <?php echo ($showRegisterLink)?'checked':''?> onchange="registerTextShown(this)" onclick="registerTextShown(this)" /> 
+<?php echo t('Show Register Link') ?>
 </div>
 
-<div id="registerTextWrap" style=" display:<?php  echo ($bObj->showRegisterLink)?'block':'none'?>; margin-top:8px; "> 
-<input name="registerText" type="text" value="<?php  echo $bObj->registerText?>" maxlength="255" />
-</div>
-
-<div style="margin-top:16px; ">
-<input name="returnToSamePage" type="checkbox" value="1" <?php  echo ($bObj->returnToSamePage)?'checked':''?> />  
-<?php  echo t('When login is complete, return users to this page.')?>
+<div id="registerTextWrap" style=" display:<?php echo ($showRegisterLink)?'block':'none'?>; margin-top:8px; "> 
+<input name="registerText" type="text" value="<?php echo $registerText?>" maxlength="255" />
 </div>
 
 <div style="margin-top:16px; ">
-<input name="hideFormUponLogin" type="checkbox" value="1" <?php  echo ($bObj->hideFormUponLogin)?'checked':''?> />  
-<?php  echo t('Hide login &amp; password fields if user is logged in.')?>
+<input name="returnToSamePage" type="checkbox" value="1" <?php echo ($returnToSamePage)?'checked':''?> />  
+<?php echo t('When login is complete, return users to this page.')?>
+</div>
+
+<div style="margin-top:16px; ">
+<input name="hideFormUponLogin" type="checkbox" value="1" <?php echo ($hideFormUponLogin)?'checked':''?> />  
+<?php echo t('Hide login &amp; password fields if user is logged in.')?>
 </div>
 
 
--- packages/login/controller.php
+++ packages/login/controller.php
@@ -1,12 +1,11 @@
 <?php 
-
 defined('C5_EXECUTE') or die(_("Access Denied."));
 
 class LoginPackage extends Package {
 
 	protected $pkgHandle = 'login';
 	protected $appVersionRequired = '5.3.3'; 
-	protected $pkgVersion = '1.1';
+	protected $pkgVersion = '1.1.1';
 	
 	public function getPackageDescription() {
 		return t("Lets you add a login box as a block.");
