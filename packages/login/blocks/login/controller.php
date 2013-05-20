0445aa2a9a186328b9f68bbcfc16b5748f79c639
Mike Lay <michael.lay.email@gmail.com>
2013-05-20 14:12:40 -0700

    Indentation whitespace cleanup

--- packages/login/blocks/login/controller.php
+++ packages/login/blocks/login/controller.php
@@ -1,45 +1,46 @@
 <?php  
-	defined('C5_EXECUTE') or die(_("Access Denied."));
-	class LoginBlockController extends BlockController {
+defined('C5_EXECUTE') or die(_("Access Denied."));
 
-		protected $btTable = 'btLogin';
-		protected $btInterfaceWidth = "400";
-		protected $btInterfaceHeight = "160";
+class LoginBlockController extends BlockController {
 
-		/**
-		 * Used for localization. If we want to localize the name/description we have to include this
-		 */
-		public function getBlockTypeDescription() {
-			return t("Lets you add a login box as a block.");
-		}
+	protected $btTable = 'btLogin';
+	protected $btInterfaceWidth = "400";
+	protected $btInterfaceHeight = "160";
 
-		public function getBlockTypeName() {
-			return t("Login Box");
-		}
-
-		public function view() {
-			$this->set('form', Loader::helper('form'));
-			if (!$this->registerText) {
-				$this->registerText=t('Click here to register %s', '&raquo;');
-			}
-			$this->set('registerText', $this->registerText);	
-			$this->set('showRegisterLink', $this->showRegisterLink); 
-			$this->set('returnToSamePage', $this->returnToSamePage);
-
-			$c = $this->getCollectionObject();
-			if ($c->isEditMode()) {
-				$this->set('hideFormUponLogin', false); 
-			} else {
-				$this->set('hideFormUponLogin', $this->hideFormUponLogin); 
-			}
-		}
-
-		public function save($data) {
-			$args['registerText'] = isset($data['registerText']) ? trim($data['registerText']) : '';
-			$args['showRegisterLink'] = intval($data['showRegisterLink']);	
-			$args['returnToSamePage'] = intval($data['returnToSamePage']);
-			$args['hideFormUponLogin'] = intval($data['hideFormUponLogin']);
-			parent::save($args);
-		}
-
+	/**
+	 * Used for localization. If we want to localize the name/description we have to include this
+	 */
+	public function getBlockTypeDescription() {
+		return t("Lets you add a login box as a block.");
 	}
+
+	public function getBlockTypeName() {
+		return t("Login Box");
+	}
+
+	public function view() {
+		$this->set('form', Loader::helper('form'));
+		if (!$this->registerText) {
+			$this->registerText=t('Click here to register %s', '&raquo;');
+		}
+		$this->set('registerText', $this->registerText);
+		$this->set('showRegisterLink', $this->showRegisterLink);
+		$this->set('returnToSamePage', $this->returnToSamePage);
+
+		$c = $this->getCollectionObject();
+		if ($c->isEditMode()) {
+			$this->set('hideFormUponLogin', false);
+		} else {
+			$this->set('hideFormUponLogin', $this->hideFormUponLogin);
+		}
+	}
+
+	public function save($data) {
+		$args['registerText'] = isset($data['registerText']) ? trim($data['registerText']) : '';
+		$args['showRegisterLink'] = intval($data['showRegisterLink']);
+		$args['returnToSamePage'] = intval($data['returnToSamePage']);
+		$args['hideFormUponLogin'] = intval($data['hideFormUponLogin']);
+		parent::save($args);
+	}
+
+}
